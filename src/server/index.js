3978dad1d2bd6d68285611f3223b8917547ac30d
sabinaasayeed <59181067+sabinaasayeed@users.noreply.github.com>
2020-05-05 12:28:29 -0400

    Add files via upload

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,3 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -8,19 +5,7 @@ const app = express()
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-console.log(`Your API key is ${process.env.API_KEY}`);
-
-var aylien = require("aylien_textapi");
-
-// set aylien API credentias
-// set aylien API credentials
-// NOTICE that textapi is the name I used, but it is arbitrary.
-// You could call it aylienapi, nlp, or anything else, 
-//   just make sure to make that change universally!
-var textapi = new aylien({
-    application_id: process.env.API_ID,
-    application_key: process.env.API_KEY
-});
+let trips = [];
 
 app.use(express.static('dist'))
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,8 +16,6 @@ console.log(__dirname)
 
 app.get('/', function(req, res) {
     res.sendFile('dist/index.html')
-        //res.sendFile('/dist/index.html', { root: __dirname + '/../..' })
-        //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
@@ -40,21 +23,13 @@ app.listen(3040, function() {
     console.log('Example app listening on port 3040!')
 })
 
-app.post('/test', function(req, res) {
-    console.log("POST /test is called");
-    console.log(req.body.text);
 
-    textapi.sentiment({
-        url: req.body.url
-    }, function(error, response) {
+// POST route
+app.post('/save', addTrip);
 
-        if (error) {
-            console.log(error);
-            return;
-        }
-        console.log(response);
-        res.json(response);
-    });
-
-
-})
+function addTrip(req, res) {
+    console.log('received save request: addTrip');
+    trips.push(req.body)
+    console.log(trips);
+    res.send(trips);
+};
