47b0fadbdab2e6ea4e12d285d30b30b12cc472ed
sabinaasayeed <59181067+sabinaasayeed@users.noreply.github.com>
2020-05-05 16:37:09 -0400

    Add files via upload

--- src/client/js/__tests__/formHandlerTest.js
+++ src/client/js/__tests__/formHandlerTest.js
@@ -1,4 +1,6 @@
 import { handleSubmit } from "../formHandler";
+import { handleSave } from "../formHandler";
+
 
 
 describe("Check if the event exists", () => {
@@ -6,5 +8,11 @@ describe("Check if the event exists", () => {
     test('test handleSubmit event is defined', () => {
         expect(handleSubmit).toBeInstanceOf(Function);
     });
+
+    test('test handleSave event is defined', () => {
+        expect(handleSave).toBeInstanceOf(Function);
+    });
+
+
 
 });
