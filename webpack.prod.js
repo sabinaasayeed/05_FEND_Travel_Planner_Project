4ae37c0ed677dc17c2164e44c2545f1c4176faf2
sabinaasayeed <59181067+sabinaasayeed@users.noreply.github.com>
2020-05-06 07:34:08 -0400

    Add files via upload

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const WorkboxPlugin = require('workbox-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -37,6 +38,7 @@ module.exports = {
             filename: "./index.html",
         }),
         new WorkboxPlugin.GenerateSW(),
-        new MiniCssExtractPlugin({ filename: '[name].css' })
+        new MiniCssExtractPlugin({ filename: '[name].css' }),
+        new CleanWebpackPlugin()
     ]
 }
